b0c17c8599cfe72e03982a2849fe02b27a8dd6b9
mariaeduardx <123771597+mariaeduardx@users.noreply.github.com>
2023-05-19 11:48:18 -0300

    Add files via upload

---
+++ print.py
@@ -0,0 +1,20 @@
+print("duda", "TESTE", sep="" )
+
+print("senai")
+
+print("divo", end=".")
+
+print(1+1)
+
+print("10³ =", pow(10,3))
+
+nome = ""
+print(nome)
+nome = 1
+print(nome)
+nome = 1+1
+print(nome)
+nome = True
+print(nome)
+
+
